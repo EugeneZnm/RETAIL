4e233a047b55aee0f6a9fdd9958375a3a6565610
EugeneZnm <eugenenzioki@gmail.com>
2019-06-28 23:33:00 +0300

    additon of brands

--- store/models.py
+++ store/models.py
@@ -1,10 +1,39 @@
 from django.db import models
 from django.conf import settings
 
+CATEGORY_CHOICES = (
+    ('LC','Laptops & Computers'),
+    ('ST','Smartphones & Tablets'),
+    ('G','Gadgets'),
+    ('A','Accessories')
+)
+
+LABEL_CHOICES = (
+    ('P','Primary'),
+    ('S','Secondary'),
+    ('D','danger'),
+)
+
+BRAND_CHOICES = (
+    ('A', 'Apple'),
+    ('G', 'Google'),
+    ('X', 'Xiaomi'),
+    ('SG', 'Samsung'),
+    ('N', 'Nokia'),
+    ('O+', 'OnePlus'),
+    ('Op', 'Oppo'),
+    ('BB', 'Blackberry'),
+    ('SY', 'Sony'),
+    ('H', 'Huawei'),
+)
+
 # Create your models here.
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES,max_length=100)
+    brand = models.CharField(choices=BRAND_CHOICES,max_length=20)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=100)
 
     def __str__(self):
         return self.title
