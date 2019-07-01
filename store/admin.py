064fb322f2b34de7637116ec170f4ef7c6d64884
EugeneZnm <eugenenzioki@gmail.com>
2019-07-01 13:49:27 +0300

    registeriung models in admin

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Item, OrderItem, Order
+from .models import Item,Category 
 
-admin.site.register(Item)
-admin.site.register(OrderItem)
-admin.site.register(Order)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Category, CategoryAdmin)
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['item', 'slug', 'price', 'stock', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug', ('item',)}
+admin.site.register(Item, ItemAdmin)
