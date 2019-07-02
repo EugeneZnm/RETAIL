ecb36141697bae8cee793a1e083f2a44f3447c7e
EugeneZnm <eugenenzioki@gmail.com>
2019-07-02 15:06:38 +0300

    addition of multiple images

--- store/models.py
+++ store/models.py
@@ -20,6 +20,8 @@ class Item(models.Model):
     title = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image2 = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image3 = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     is_featured = models.BooleanField(default=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -36,4 +38,4 @@ class Item(models.Model):
         index_together = (('id', 'slug'),)
         
     def __str__ (self):
-        return self.name    
+        return self.title   
