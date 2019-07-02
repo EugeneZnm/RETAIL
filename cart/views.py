7e5f88226663939ea3f18ddb25f47949fec8e8c1
EugeneZnm <eugenenzioki@gmail.com>
2019-07-02 17:09:10 +0300

    view function for updating cart and adding items to cart

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators .http import require_POST
+from store.models import Item
+from .cart import Cart
+from .forms import CartAddItemForm
+# Create your views here.#
 
-# Create your views here.
+@require_POST
+def cart_add(request, item_id):
+    
+    # adding products to cart nd updating quantities of existing products
+    cart = Cart(request)
+    item = get_object_or_404(Item, id=item_id)
+    form = CartAddItemForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(item=item, quantity=cd['quantity'],update_quantity=cd['update'])
+    return redirect('cart: cart_detail')    
