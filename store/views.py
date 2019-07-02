34d92b6bb87a723a1bc3154f57b2c5860af2643b
EugeneZnm <eugenenzioki@gmail.com>
2019-07-02 12:54:39 +0300

    getting available products and filter by a single category

--- store/views.py
+++ store/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
-from .models import Item
+from .models import Item, Category
 
 # Create your views here.
 def home(request):
@@ -20,3 +19,17 @@ def product(request, item_id):
         'item': item,
     }    
     return render (request, "product.html", context)
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+
+    # filter to get only available products
+    prodcuts = Item.objects.filter(available=True)
+    # filter products by a single category
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render (request, 'shop.html', {'category': category, 'categories': categories, 'products': products})    
+
+    
