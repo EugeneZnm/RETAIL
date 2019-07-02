8a474cd415d35e55d63eb22ee9f07e8d4da0e792
EugeneZnm <eugenenzioki@gmail.com>
2019-07-02 15:51:28 +0300

    addtion of method to add product to the cart and updating and saving th cart session

--- cart/cart.py
+++ cart/cart.py
@@ -21,3 +21,25 @@ class Cart(object):
             # saving an empty cart in the session if no cart is present. Product id used as keys in dictionary and quantity and price as value for each key to guarantee a product is not addded more than once in the cart
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+
+    def add(self, product, quantity=1, update_quantity=False):
+
+        """
+        addition of a product to the cart and updating its quantity
+        """    
+        product_id = str(product_id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()           
+
+    def save(self):
+        # updating of the session cart
+        self.session[settings.CART_SESSION_ID] = self.cart
+
+        # marking the session as modified
+        self.session.modified =True 
