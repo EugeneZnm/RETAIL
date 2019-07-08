f1b3641970c2979d626d6974032b9b3f9da1c9c8
EugeneZnm <eugenenzioki@gmail.com>
2019-07-08 12:48:43 +0300

    adding add to cart fotm to product2

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Item, Category
+from cart.forms import CartAddItemForm
 
 # Create your views here.
 def home(request):
@@ -26,14 +27,16 @@ def product_list(request, category_slug=None):
     categories = Category.objects.all()
 
     # filter to get only available products
-    prodcuts = Item.objects.filter(available=True)
+    items = Item.objects.filter(available=True)
     # filter products by a single category
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
-    return render (request, 'shop.html', {'category': category, 'categories': categories, 'products': products})    
+        items = items.filter(category=category)
+    return render (request, 'shop.html', {'category': category, 'categories': categories, 'items': items})    
 
 # Retrieving a single product
 def product_detail(request, id ,slug):
-    product = get_object_or_404(Item, id=id, slug=slug,available=True)
-    return render(request, 'product.html', {'product': product})    
+    item = get_object_or_404(Item, id=id, slug=slug,available=True)
+
+    cart_product_form = CartAddItemForm()
+    return render(request, 'product.html', {'item': item, 'cart_product_form': cart_product_form})    
