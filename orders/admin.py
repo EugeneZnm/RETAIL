70bdebf461e8832c455eb03d3ce0daa62b05a210
EugeneZnm <eugenenzioki@gmail.com>
2019-07-08 18:44:43 +0300

    addition of order models in admin dashboard

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Order, OrderItem
 # Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    """
+    model included inline in admin dashboard, allowing it to appear in the same edit page as the parent model
+    """
+    model = OrderItem
+    raw_id_fields = ['product']
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated']
+    list_filter = ['paid', 'created', 'updated']
+    inline = [OrderItemInline] 
+
+admin.site.register(Order, OrderAdmin)       
